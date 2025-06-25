56d2b24df71472daaaa68cc8d6e4adf5708de919
MauriAGM <gutierrezmusajamauricio@gmail.com>
2025-06-25 15:00:30 -0500

    Avance 2 : Implementadas funciones para agregar, eliminar y mostrar contactos registrados usando estructuras y arreglo fijo

--- gestion_contactos.cpp
+++ gestion_contactos.cpp
@@ -23,16 +23,67 @@ struct contactoEmail
     string Nacionalidad;
 };
 
-void agregarcontacto(){
-
+contactoEmail agregarcontacto(){
+    contactoEmail contactos;
+    cin.ignore();
+    cout << "Ingrese el nombre ";
+    getline(cin,contactos.Nombres);
+    cout << "Ingrese los apellidos: ";
+    getline(cin, contactos.Apellidos);
+    cout << "Ingrese el sexo: ";
+    getline(cin, contactos.Sexo);
+    cout << "Ingrese la edad de la persona: ";
+    cin >> contactos.Edad;
+    cin.ignore();
+    cout << "Ingrese el telefono de la persona: ";
+    getline(cin,contactos.Telefono);
+    cout << "Ingrese el email del contacto: ";
+    getline(cin,contactos.Email);
+    cout << "Ingrese la nacionalidad de la persona: ";
+    getline(cin, contactos.Nacionalidad);
+    return contactos;
 }
 
-void eliminarcontacto(){
-
+void agregarcontaco(contactoEmail contactos[],int &TAM, int &MAX){
+    contactos[TAM] = agregarcontacto();
+    TAM++;
+    MAX++;
 }
-void contactosregistrados(){
 
+void eliminarcontacto(contactoEmail contactos[], int &TAM){
+    bool encontrado = false;
+    string buscar_email;
+    cout << "Ingrese el email del contacto a eliminar: ";
+    getline(cin, buscar_email);
+    for (int i = 0; i < TAM; i++){
+        if (contactos[i].Email == buscar_email){
+            for (int j = 0; i < TAM - 1; j++){
+                contactos[j] = contactos[j + 1];
+            }
+            TAM --; 
+            encontrado = true;
+            cout << "Contacto eliminado. " << endl;
+        }
+    }
+    if (encontrado == false){
+        cout << "No se encontro el contacto" << endl;
+    }
 }
+void contactosregistrados(contactoEmail contactos[], int TAM){
+    cout << "Contactos registrados: " << endl; 
+    for (int i = 0; i < TAM; i++) {
+        cout << " [ " << i + 1 << " ] ";
+        cout << "Nombre: " << contactos[i].Nombres << endl;
+        cout << "Apellido: " << contactos[i].Apellidos << endl;
+        cout << "Sexo: " << contactos[i].Sexo << endl;
+        cout << "Edad: " << contactos[i].Edad << endl;
+        cout << "Telefono: " << contactos[i].Telefono << endl;
+        cout << "Email: " << contactos[i].Email << endl;
+        cout << "Nacionalidad: " << contactos[i].Nacionalidad << endl;
+    }
+    cout << endl;
+}
+
 void contactosexistentes(){
 
 }
@@ -41,36 +92,36 @@ void mensajesalida(){
 }
 
 void menu(){
-    cout << "1. Agregar un contacto" << endl;
-    cout << "2. Eliminar un contacto" << endl;
-    cout << "3. Mostrar contactos registrados" << endl;
-    cout << "4. Mostrar contactos existentes" << endl;
-    cout << "5. Salir del programa" << endl;
+    int MAX = 1;
+    contactoEmail contactos[MAX];
+    int TAM = 0;
     int opcion;
     do {
+        cout << "1. Agregar un contacto" << endl;
+        cout << "2. Eliminar un contacto" << endl;
+        cout << "3. Mostrar contactos registrados" << endl;
+        cout << "4. Mostrar contactos existentes" << endl;
+        cout << "5. Salir del programa" << endl;
         cin >> opcion;
         switch (opcion){
             case 1 :
-                agregarcontacto();
+                agregarcontaco(contactos, TAM, MAX); 
                 break;
             case 2 :
-                eliminarcontacto();
+                eliminarcontacto(contactos, TAM);
                 break;
             case 3 : 
-                contactosregistrados();
+                contactosregistrados(contactos, TAM);
                 break;
             case 4 : 
                 contactosexistentes();
                 break;
             case 5 :
                 mensajesalida();
-            default:
-                cout <<  "ERROR" ;
-        }
+        }   
 
     } while (opcion != 5);
 }
-
 int main(){
     menu();
     return 0;
